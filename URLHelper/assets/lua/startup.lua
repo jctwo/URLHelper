d872fa5bca0201af357ca3543c83dc78a0a38348
Chris Emerson <github@mail.nosreme.org>
2013-01-28 22:16:28 +0000

    A bit more Lua.

--- URLHelper/assets/lua/startup.lua
+++ URLHelper/assets/lua/startup.lua
@@ -1,5 +1,23 @@
 local _G = _G
 
-Toast = luajava.bindClass("android.widget.Toast")
-t = Toast:makeText(ctx, "Test toast", Toast.LENGTH_LONG)
-t:show()
+do
+    local Toast = luajava.bindClass("android.widget.Toast")
+    local PreferenceManager = luajava.bindClass("android.preference.PreferenceManager")
+    local SharedPrefs = PreferenceManager:getDefaultSharedPreferences(ctx)
+    
+    function _G.toast(msg)
+      t = Toast:makeText(ctx, msg, Toast.LENGTH_LONG)
+      t:show()
+    end
+    
+    _G.prefs = setmetatable({},
+                            {
+                               __index = function(t, key)
+                                   -- Should the default be different?
+                                   return SharedPrefs:getString(key, "")
+                                end
+                            })                                 
+end
+
+x = prefs["launchimm"]
+toast(x)
