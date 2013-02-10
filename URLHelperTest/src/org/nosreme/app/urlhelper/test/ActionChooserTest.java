557989e16e01917646da78642d3c91020974a54c
Chris Emerson <github@mail.nosreme.org>
2013-02-10 21:55:21 +0000

    Add a basic "cancel" test.

--- URLHelperTest/src/org/nosreme/app/urlhelper/test/ActionChooserTest.java
+++ URLHelperTest/src/org/nosreme/app/urlhelper/test/ActionChooserTest.java
@@ -16,6 +16,16 @@ import android.widget.Button;
 public class ActionChooserTest extends
 		ActivityInstrumentationTestCase2<ActionChooser> {
 	
+	/* Simple class for returning the full result from an activity. */
+	public class ActivityResult {
+		public int code;
+		public Intent data;
+		public ActivityResult(int c, Intent d) {
+			code = c;
+			data = d;
+		}
+	}
+	
 	public ActionChooserTest() {
 		super("org.nosreme.app.urlhelper", ActionChooser.class);
 	}
@@ -31,6 +41,30 @@ public class ActionChooserTest extends
 	public void testPreConditions() {
 	    /* Nothing yet to test */
 	}
+
+	protected ActivityResult getResult(Activity activity)
+	{
+		/* Thanks to 
+		 * http://stackoverflow.com/questions/5569830/get-result-from-an-activity-after-finish-in-an-android-unit-test
+		 * for this way of finding the activity result.  There *must* be a
+		 * better way... */
+		try {
+			Field f = Activity.class.getDeclaredField("mResultCode");
+			f.setAccessible(true);
+			int actualResultCode = (Integer)f.get(activity);
+			f = Activity.class.getDeclaredField("mResultData");
+			f.setAccessible(true);
+			Intent realResult = (Intent)f.get(activity);
+			return new ActivityResult(actualResultCode, realResult);
+		} catch (NoSuchFieldException e) {
+			assert false;
+			return null;
+		} catch (Exception e) {
+			assert false;
+			return null;
+		}
+		
+	}
 	
 	public void testSimple() {
 		Context context = this.getInstrumentation().getTargetContext().getApplicationContext();
@@ -40,10 +74,6 @@ public class ActionChooserTest extends
 	    
 	    ActionChooser activity = getActivity();
 	    
-	    /* Thanks to 
-             * http://stackoverflow.com/questions/5569830/get-result-from-an-activity-after-finish-in-an-android-unit-test
-	     * for this way of finding the activity result.  There *must* be a
-	     * better way... */
 	    final Button okButton = (Button)activity.findViewById(org.nosreme.app.urlhelper.R.id.choose_open);
 	    
 	    //okButton.requestFocus();
@@ -54,19 +84,26 @@ public class ActionChooserTest extends
 		});
 	    
 		getInstrumentation().waitForIdleSync();
-	  
-		try {
-			Field f = Activity.class.getDeclaredField("mResultCode");
-			f.setAccessible(true);
-			int actualResultCode = (Integer)f.get(getActivity());
-			assertEquals(actualResultCode, Activity.RESULT_OK);
-			f = Activity.class.getDeclaredField("mResultData");
-			f.setAccessible(true);
-			Intent realResult = (Intent)f.get(getActivity());
-		} catch (NoSuchFieldException e) {
-			assert false;
-		} catch (Exception e) {
-			assert false;
-		}
+		
+		ActivityResult result = getResult(activity);
+		assertEquals(result.code, Activity.RESULT_OK);	  
+	}
+	
+	public void testCancel() {
+		Context context = this.getInstrumentation().getTargetContext().getApplicationContext();
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.example.com/"), context, ActionChooser.class);
+		
+	    setActivityIntent(intent);
+
+	    ActionChooser activity = getActivity();
+	    getInstrumentation().waitForIdleSync();
+
+	    sendKeys(KeyEvent.KEYCODE_BACK);
+
+	    getInstrumentation().waitForIdleSync();
+
+		ActivityResult result = getResult(activity);
+		assertEquals(result.code, Activity.RESULT_CANCELED);	  
+
 	}
 }
