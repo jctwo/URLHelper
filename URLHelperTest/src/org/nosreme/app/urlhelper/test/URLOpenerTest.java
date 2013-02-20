695b629e85702d195ec096480c036dead0f09d2d
Chris Emerson <github-m@mail.nosreme.org>
2013-02-20 21:54:24 +0000

    Add so far empty test class.

---
+++ URLHelperTest/src/org/nosreme/app/urlhelper/test/URLOpenerTest.java
@@ -0,0 +1,18 @@
+package org.nosreme.app.urlhelper.test;
+import android.test.*;
+import org.nosreme.app.urlhelper.*;
+
+public class URLOpenerTest extends ActivityUnitTestCase<URLOpenActivity>
+
+{
+    public URLOpenerTest() {
+	super(URLOpenActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+	super.setUp();
+
+    }
+
+}
