36a28f045fa6e1563056299bfe76d7e510d37946
Chris Emerson <github-m@mail.nosreme.org>
2013-05-28 22:18:16 +0100

    Make prefs writable in Lua, and add a rule store based on it.

--- URLHelper/assets/lua/startup.lua
+++ URLHelper/assets/lua/startup.lua
@@ -15,15 +15,55 @@ do
                                __index = function(t, key)
                                    -- Should the default be different?
                                    return SharedPrefs:getString(key, "")
-                                end
+                                end,
+                                __newindex = function(t, k, v)
+                           
+                                   editor = SharedPrefs:edit()
+                                   editor:putString(k, v)
+                                   editor:commit()
+                                end,
                             })
-    function _G.getStore()
-         local urlStore = luajava.new(UrlStore, ctx)
-         return urlStore
-    end
+    
+    local urlStoreTab = load("return " .. prefs["luaStore"] or "{}",
+                             "Rule table",
+                             "t", {})()
+    urlStore = setmetatable({
+      __index = urlStore,
+      __newindex = function(t, k, v)
+          urlStoreTab[k] = v
+          prefs["luaStore"] = tostring(urlStoreTab)
+      end,
+      __tostring = function(t)
+          local res = {}
+          local function add(s)
+              table.insert(res, s)
+          end
+          
+          add("{")
+          
+          for i,v in ipairs(urlStoreTab) do
+              add("{\"" .. v[1] .. "\", \"" .. v[2] .. "\"}")
+          end
+          
+          add("}")
+          return table.concat(res, ", ")
+          
+      end,
+    }, urlStoreTab)
 end
 
 x = prefs["launchimm"]
-toast("Current mode: " .. x)
-store = getStore()
-toast(store:getUrl(1))
+-- toast("Current mode: " .. x)
+
+--toast(store:getUrl(1))
+
+y = prefs["luatest"]
+if string.len(y) > 0 then
+  y = y + 1
+else
+  y = 1
+end
+y = tostring(y)
+prefs["luatest"] = y
+toast(y)
+
