623c57c47c129fdf346e4d8c4633f57b945b9094
Chris Emerson <github-m@mail.nosreme.org>
2013-06-17 09:03:34 +0100

    Add a second button, and no window for LuaActivity.

--- URLHelper/src/org/nosreme/app/urlhelper/LuaAppWidgetProvider.java
+++ URLHelper/src/org/nosreme/app/urlhelper/LuaAppWidgetProvider.java
@@ -3,6 +3,7 @@ import android.appwidget.*;
 import android.content.*;
 import android.app.*;
 import android.widget.*;
+import android.net.Uri;
 
 public class LuaAppWidgetProvider extends AppWidgetProvider
 {
@@ -16,11 +17,21 @@ public class LuaAppWidgetProvider extends AppWidgetProvider
 	    int appWidgetId = appWidgetIds[i];
 	    // Create an Intent to launch ExampleActivity
 	    Intent intent = new Intent(context, LuaActivity.class);
+	    intent.setData(Uri.parse("lua://testact"));
 	    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 	    // Get the layout for the App Widget and attach an on-click listener
 	    // to the button
 	    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.url_controlwidget);
 	    views.setOnClickPendingIntent(R.id.luabutton, pendingIntent);
+	    // Tell the AppWidgetManager to perform an update on the current app widget
+	    appWidgetManager.updateAppWidget(appWidgetId, views);
+
+	    intent.setData(Uri.parse("lua://widget"));
+	    pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+	    // Get the layout for the App Widget and attach an on-click listener
+	    // to the button
+	    views = new RemoteViews(context.getPackageName(), R.layout.url_controlwidget);
+	    views.setOnClickPendingIntent(R.id.luabutton2, pendingIntent);
 	    // Tell the AppWidgetManager to perform an update on the current app widget
 	    appWidgetManager.updateAppWidget(appWidgetId, views);
 	}
